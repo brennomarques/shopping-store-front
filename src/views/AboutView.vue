b09e43ef7cfb9958de6f1a31b4c59edc03279425
Brenno lima <brenno.marquesb@gmail.com>
2024-05-01 11:42:57 -0300

    setup: first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount } from 'vue'
+
+onBeforeMount(() => {
+  console.log(import.meta.env.VITE_BASE_URL)
+})
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
