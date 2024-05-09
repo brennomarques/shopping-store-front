e96a63c3e6ed45a3f55061987e57dbc20142344d
Brenno lima <brenno.marquesb@gmail.com>
2024-05-09 10:26:24 -0300

    chore: created auth router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
 import ProductsView from '@/views/pages/ProductsView.vue';
 import Page404View from '@/views/pages/error/Page404View.vue';
 import AboutView from '@/views/pages/AboutView.vue';
@@ -7,9 +6,6 @@ import DashboardView from '@/views/pages/DashboardView.vue';
 import LoginPageView from '@/views/pages/auth/LoginPageView.vue';
 import { Authorization } from '@/core/services';
 import SignUpPageView from '@/views/pages/auth/SignUpPageView.vue';
-import OrderView from '@/views/pages/OrderView.vue';
-import ListProductView from '@/views/pages/ListProductView.vue';
-import ProfileView from '@/views/pages/ProfileView.vue';
 import MyShoppingView from '@/views/pages/MyShoppingView.vue';
 
 const router = createRouter({
@@ -19,96 +15,80 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: LoginPageView
+      component: LoginPageView,
+      meta: { requiresAuth: false }
     },
     {
       path: '/register',
       name: 'register',
-      component: SignUpPageView
+      component: SignUpPageView,
+      meta: { requiresAuth: false }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+      meta: { requiresAuth: false }
+    },
+    
+    /**Router Required authentication */
+    {
+      path: '',
+      redirect: '/dashboard',
+      meta: { requiresAuth: true }
     },
     {
       path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardView
+      component: DashboardView,
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: '/',
+          redirect: 'dashboard',
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: DashboardView,
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'shopping',
+          name: 'shopping',
+          component: MyShoppingView,
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'product',
+          name: 'product',
+          component: ProductsView,
+          meta: { requiresAuth: true },
+        },
+      ]
     },
-    {
-      path: '/order',
-      name: 'order',
-      component: OrderView
-    },
-    {
-      path: '/product',
-      name: 'product',
-      component: ListProductView
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView 
-    },
-    {
-      path: '/shopping',
-      name: 'shopping',
-      component: MyShoppingView
-    },
-    // {
-    //   path: '/',
-    //   redirect: '/dashboard'
-    // },
-    // {
-    //   path: '/dashboard',
-    //   component: DashaboardView,
-    //   children: [
-    //     {
-    //       path: '',
-    //       redirect: 'dashboard'
-    //     },
-    //     {
-    //       path: 'dashboard',
-    //       name: 'dashboard',
-    //       component: DashaboardView
-    //     },
-    //     {
-    //       path: 'shopping',
-    //       name: 'shopping',
-    //       component: ShoppingView
-    //     },
-    //     {
-    //       path: 'product',
-    //       name: 'product',
-    //       component: ProductsView
-    //     },
-    //     {
-    //       path: 'about',
-    //       name: 'about',
-    //       component: AboutView
-    //     }
-    //   ]
-    // },
     {
       path: '/:pathMatch(.*)*',
       name: 'error404',
-      component: Page404View
+      component: Page404View,
+      meta: { requiresAuth: false },
     }
   ]
 });
 
-// // Guarda de navegação global para verificar autenticação
-// router.beforeEach((to, from, next) => {
-//   console.log('new Authorization().isAuthenticated()');
-//   console.log(new Authorization().isAuthenticated());
 
-//   if (to.name === 'login' && new Authorization().isAuthenticated()) {
-//     next('/dashboard'); // Redirecionar usuário logado para a dashboard se tentar acessar a página de login
-//   } else if (to.matched.some(record => record.meta.requiresAuth) && !new Authorization().isAuthenticated()) {
-//     next('/login'); // Redirecionar usuário não autenticado para a página de login se tentar acessar uma rota protegida
-//   } else {
-//     next(); // Continuar a navegação normalmente
-//   }
-//   // router.beforeEach((to, from, next) => {
-//   //   if (to.name !== 'Login' && !isAuthenticated()) next({ name: 'Login' });
-//   //   else next();
-//   // });
-// });
+router.beforeEach((to, from, next) => {
+
+  if ((to.name === 'login' || to.name === 'register') && Authorization.isAuthenticated()) {
+    next('/dashboard');
+    return;
+  } 
+  if (to.matched.some(record => record.meta.requiresAuth) && !Authorization.isAuthenticated()) {
+    next('/login');
+    return;
+  } 
+  next();
+  
+});
 
 export default router;
